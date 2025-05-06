1284886d32a298ce798522cab3243703921bbb75
小桂花 <143058334+guihuala@users.noreply.github.com>
2025-05-06 17:33:01 +0800

    修改对话地块代码

--- Assets/Scripts/Trigger/DialogueTrigger.cs
+++ Assets/Scripts/Trigger/DialogueTrigger.cs
@@ -5,16 +5,35 @@ using UnityEngine;
 public class DialogueTrigger : MonoBehaviour, IEnterSpecialItem
 {
     [SerializeField] private DialogueData dialogueData;
+    [SerializeField] private List<DialogueTrigger> linkedTriggers; // 联动触发器列表
     private bool isPlayed = false;
+    private bool isDisabledByLink = false; // 是否被其他联动触发器禁用
 
     public void Apply()
     {
-        if (isPlayed)
+        // 如果已经播放过或被其他触发器禁用，则返回
+        if (isPlayed || isDisabledByLink)
             return;
 
+        // 触发所有联动触发器，将它们标记为禁用
+        foreach (var trigger in linkedTriggers)
+        {
+            if (trigger != this) // 避免自引用
+            {
+                trigger.DisableByLinkedTrigger();
+            }
+        }
+
+        // 开始对话
         DialoguePanel dialoguePanel = UIManager.Instance.OpenPanel("DialoguePanel") as DialoguePanel;
         dialoguePanel.StartDialogue(dialogueData);
         
         isPlayed = true;
+    }
+
+    // 被其他联动触发器调用，禁用此触发器
+    public void DisableByLinkedTrigger()
+    {
+        isDisabledByLink = true;
     }
 }

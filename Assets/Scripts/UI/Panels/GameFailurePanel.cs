d0f63a281bb6a86ca1bea13d6608065c76c4442e
小桂花 <143058334+guihuala@users.noreply.github.com>
2025-05-20 12:58:36 +0800

    不同主题的关卡失败图

--- Assets/Scripts/UI/Panels/GameFailurePanel.cs
+++ Assets/Scripts/UI/Panels/GameFailurePanel.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using DG.Tweening;
 using UnityEngine;
+using UnityEngine.Serialization;
 using UnityEngine.UI;
 
 public class GameFailurePanel : BasePanel
@@ -10,10 +11,32 @@ public class GameFailurePanel : BasePanel
     [SerializeField] private Button replayBtn;
     [SerializeField] private Button mainMenuBtn;
     [SerializeField] private Text titleText;
+    
+    [SerializeField] private Image FailureImg;
+    [SerializeField] private Sprite[] FailureSprites;
 
     public override void OpenPanel(string name)
     {
         base.OpenPanel(name);
+        // 获取当前关卡信息
+        LevelInfo levelInfo = FindObjectOfType<LevelInfo>();
+        if (levelInfo != null)
+        {
+            int index = 0;
+            if (levelInfo.currentScene == SceneName.Level1_1 || levelInfo.currentScene == SceneName.Level1_2 ||
+                levelInfo.currentScene == SceneName.Level1_3 || levelInfo.currentScene == SceneName.Level1_4)
+                index = 0;
+            
+            else if (levelInfo.currentScene == SceneName.Level2_1 || levelInfo.currentScene == SceneName.Level2_2 ||
+                     levelInfo.currentScene == SceneName.Level2_3 || levelInfo.currentScene == SceneName.Level2_4)
+                index = 1;
+            
+            else if (levelInfo.currentScene == SceneName.Level3_1 || levelInfo.currentScene == SceneName.Level3_2 ||
+                     levelInfo.currentScene == SceneName.Level3_3 || levelInfo.currentScene == SceneName.Level3_4)
+                index = 2;
+            
+            SetFailureImage(index); // 传入当前关卡索引
+        }
         
         DOTween.Sequence()
             .AppendInterval(0.5f)
@@ -49,5 +72,28 @@ public class GameFailurePanel : BasePanel
             
             SceneLoader.Instance.LoadScene(SceneName.LevelSelection, "...");
         });
+    }
+    
+    /// <summary>
+    /// 根据关卡索引设置胜利界面的图片
+    /// </summary>
+    private void SetFailureImage(int levelIndex)
+    {
+        if (FailureSprites != null && FailureSprites.Length > 0)
+        {
+            // 确保索引在有效范围内
+            if (levelIndex >= 0 && levelIndex < FailureSprites.Length)
+            {
+                FailureImg.sprite = FailureSprites[levelIndex];
+            }
+            else
+            {
+                FailureImg.sprite = FailureSprites[0]; // 默认显示第一张图片
+            }
+        }
+        else
+        {
+            Debug.LogError("VictorySprites 数组为空，无法设置胜利图片");
+        }
     }
 }
